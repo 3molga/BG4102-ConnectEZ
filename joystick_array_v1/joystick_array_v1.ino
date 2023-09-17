518d87335c358be8f7e9c0f475be425f762f2624
Thad Lo <ihack2win@gmail.com>
2023-09-17 20:29:15 +0800

    Added new folder for developing grid array stuff

--- joystick_array_v1/joystick_array_v1.ino
+++ joystick_array_v1/joystick_array_v1.ino
@@ -9,16 +9,17 @@
 #include "ezButton.h"
 #include <iostream>
 #include <array>
+#include <string>
 using namespace std;
 
 // Creating objects
 joystick joystick(13, 12);
-ezButton buttonConfirm();
+ezButton buttonConfirm(); // Fill this in later ya dumb cunt
 ezButton buttonReturn();
 
 
 // Defining variables
-std::array(int, 2) userInputs;
+std::array(int, 2) userState; // Stores x and y coords of where the user currently is on the UI
 
 void setup() {
   Serial.begin(9600);
@@ -56,4 +57,40 @@ void loop() {
   userInputs = joystick.joystickReturnState();
   Serial.println(joystick.joystickMessageCheck());
   Serial.println(userInputs);
+  updateInputs(userInputs);
+  Serial.println(printUserState(userState))
+}
+
+//------------------------------------FUNCTIONS------------------------------------
+// Function to update user state (temporary, maybe move into class later)
+void updateInputs(std::array userInputs){
+  // Assuming the grid is 2D, get the x and y coords of the current box
+  int x_coord = userState[0];
+  int y_coord = userState[1];
+
+  // Update the coords accordingly
+  x_coord = x_coord + userInputs[0];
+  y_coord = y_coord + userInputs[1];
+
+  // Check that the values are within the bounds
+  // Will need to update this with some way to either dynamically get the grid size from some kinda object, or just define a static grid size
+  // For now just check that it doesn't go negative
+  if (x_coord < 0){
+    x_coord = 0;
+  } 
+
+  if (y_coord < 0){
+    y_coord = 0;
+  }
+
+  // Update user state
+  userState[0] = x_coord;
+  userState[1] = y_coord;
+}
+
+// Funcion to convert user state into a string to print
+std::string printUserState(std::array userState){
+  std::string tempOutput = to_string(userState[0]);
+  tempOutput = tempOutput + " , " + to_string(userState[1]);
+  return tempOutput;
 }
