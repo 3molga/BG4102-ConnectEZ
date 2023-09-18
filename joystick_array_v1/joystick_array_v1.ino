50f57db4ae8bdda0c4386975b256418668425838
Thad Lo <ihack2win@gmail.com>
2023-09-18 18:05:34 +0800

    Starting work on joystick_array_v1

--- joystick_array_v1/joystick_array_v1.ino
+++ joystick_array_v1/joystick_array_v1.ino
@@ -14,12 +14,12 @@ using namespace std;
 
 // Creating objects
 joystick joystick(13, 12);
-ezButton buttonConfirm(); // Fill this in later ya dumb cunt
-ezButton buttonReturn();
+ezButton buttonConfirm(1); // Fill this in later ya dumb cunt
+ezButton buttonReturn(2);
 
 
 // Defining variables
-std::array(int, 2) userState; // Stores x and y coords of where the user currently is on the UI
+std::array<int, 2> userState; // Stores x and y coords of where the user currently is on the UI
 
 void setup() {
   Serial.begin(9600);
@@ -54,38 +54,25 @@ void loop() {
 
   // Lowest priority state: joystick input
   // Get and print joystick state and returned message
-  userInputs = joystick.joystickReturnState();
+  userInput = joystick.joystickReturnState();
   Serial.println(joystick.joystickMessageCheck());
-  Serial.println(userInputs);
-  updateInputs(userInputs);
+  Serial.println(userInput);
+  updateInputs(userInput, 3);
   Serial.println(printUserState(userState))
 }
 
 //------------------------------------FUNCTIONS------------------------------------
 // Function to update user state (temporary, maybe move into class later)
-void updateInputs(std::array userInputs){
-  // Assuming the grid is 2D, get the x and y coords of the current box
-  int x_coord = userState[0];
-  int y_coord = userState[1];
-
-  // Update the coords accordingly
-  x_coord = x_coord + userInputs[0];
-  y_coord = y_coord + userInputs[1];
-
-  // Check that the values are within the bounds
-  // Will need to update this with some way to either dynamically get the grid size from some kinda object, or just define a static grid size
-  // For now just check that it doesn't go negative
-  if (x_coord < 0){
-    x_coord = 0;
-  } 
-
-  if (y_coord < 0){
-    y_coord = 0;
+// Takes user inputs and upper bound of array size (assume array is square for now?)
+void updateInputs(std::array<int, 2> userInput, int upper_bound){
+  for (int i = 0; i < userState.size(); i++){
+    userState[i] = userState[i] + userInput[i];
+    if (userState[i] < 0){
+      userState[i] = 0;
+    } else if (userState[i > upper_bound]){
+      userState[i] = upper_bound;
+    }
   }
-
-  // Update user state
-  userState[0] = x_coord;
-  userState[1] = y_coord;
 }
 
 // Funcion to convert user state into a string to print
