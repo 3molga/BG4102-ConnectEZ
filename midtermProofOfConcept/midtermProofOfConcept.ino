cfde622e5d10d3893b3a8255d42fdb7c0d305d7c
Thad Lo <ihack2win@gmail.com>
2023-09-27 22:50:39 +0800

    Trying multitasking

--- midtermProofOfConcept/midtermProofOfConcept.ino
+++ midtermProofOfConcept/midtermProofOfConcept.ino
@@ -19,36 +19,40 @@ using namespace std;
 
 // Input objects
 joystick joystick(32, 35);
-ezButton buttonConfirm(27);  // Fill this in later ya dumb cunt
+ezButton buttonConfirm(27); // Fill this in later ya dumb cunt
 ezButton buttonReturn(26);
 ezButton buttonTele(25);
 #define touchPinInput 13
-#define touchPinLED 12  // External LED is at Pin 26
-#define ONBOARD_LED 2   // ON_BOARD LED value is 2
+#define touchPinLED 12 // External LED is at Pin 26
+#define ONBOARD_LED 2  // ON_BOARD LED value is 2
 
 // Wifi setup
-const char *ssid = "tmt";                                          // Wi-Fi ID
-const char *password = "yeah8913";                                 // Wi-Fi password
-#define BOTtoken "6606670486:AAGP24iLfP047ysZ7yaAUHyv3cOf-iLKMqE"  // Telegram bot API token
-String CHAT_ID = "-4016407865";                                // "Chat with esp32" telegram group ID
+const char *ssid = "tmt";                                         // Wi-Fi ID
+const char *password = "yeah8913";                                // Wi-Fi password
+#define BOTtoken "6606670486:AAGP24iLfP047ysZ7yaAUHyv3cOf-iLKMqE" // Telegram bot API token
+String CHAT_ID = "-4016407865";                                   // "Chat with esp32" telegram group ID
 WiFiClientSecure client;
-UniversalTelegramBot bot(BOTtoken, client);
-telebot botController(bot);
+UniversalTelegramBot botActive(BOTtoken, client);
+UniversalTelegramBot botPassive(BOTtoken, client);
+telebot botController(botPassive);
 
 // Joystick variables
-std::vector<int> userState{ 0, 0 };       // Stores x and y coords of where the user currently is on the UI
-std::vector<int> userInput{ 0, 0 };       // Stores inputs from user
-std::vector<int> matrixSize = { 3, 10 };  // Arbitrary assumption for maximum size of array (+1)
+std::vector<int> userState{0, 0};      // Stores x and y coords of where the user currently is on the UI
+std::vector<int> userInput{0, 0};      // Stores inputs from user
+std::vector<int> matrixSize = {3, 10}; // Arbitrary assumption for maximum size of array (+1)
 bool joystickUpdateTrigger;
 const String newLineBar = "-----------------------------------------------------------------------------------------------";
 
 // Capacitance touch to detect if person is afk or not
-int touchValue;                 // Variable for storing the touch pin value
-const int touchThreshold = 30;  // Capacitance touch value lower than this will indicate the person is touching the device
+int touchValue;                // Variable for storing the touch pin value
+const int touchThreshold = 30; // Capacitance touch value lower than this will indicate the person is touching the device
 bool touchBool;
 
+TaskHandle_t Task1;
+
 // -----------------------------------------SETUP-------------------------------------------
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   joystick.joystickSetup();
   buttonConfirm.setDebounceTime(50);
@@ -60,25 +64,40 @@ void setup() {
   WiFi.disconnect();
   WiFi.begin(ssid, password);
   client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
-  while (millis() < 5000) {
-    if (WiFi.status() == WL_CONNECTED) {
+  while (millis() < 5000)
+  {
+    if (WiFi.status() == WL_CONNECTED)
+    {
       Serial.println("WiFi connected");
-      bot.sendMessage(CHAT_ID, "Hello everyone, I am connected!");
+      botActive.sendMessage(CHAT_ID, "Hello everyone, I am connected!");
       break;
-    } else {
+    }
+    else
+    {
       delay(500);
       Serial.print(".");
     }
   }
 
-  // Setup button, Wi-Fi confirmaton LED and afk LED
+  // Set up button, Wi-Fi confirmaton LED and afk LED
   pinMode(ONBOARD_LED, OUTPUT);
   pinMode(touchPinLED, OUTPUT);
   digitalWrite(touchPinLED, 0);
+
+  // Set up parallel processing task to handle Telegram reading
+  xTaskCreatePinnedToCore(
+      TelegramPollReturn, /* Task function. */
+      "TelePoll",         /* name of task. */
+      100000,             /* Stack size of task */
+      NULL,               /* parameter of the task */
+      1,                  /* priority of the task */
+      &TelePoll,          /* Task handle to keep track of created task */
+      1);                 /* pin task to core 1 */
 }
 
 // -----------------------------------------LOOP-------------------------------------------
-void loop() {
+void loop()
+{
   buttonConfirm.loop();
   buttonReturn.loop();
   buttonTele.loop();
@@ -86,31 +105,40 @@ void loop() {
 
   // Check if the touchValue is below the threshold, check if the person is touching the device
   touchValue = touchRead(touchPinInput);
-  if (touchValue < touchThreshold) {
+  if (touchValue < touchThreshold)
+  {
     touchBool = true;
     digitalWrite(touchPinLED, HIGH);
-  } else {
+  }
+  else
+  {
     touchBool = false;
     digitalWrite(touchPinLED, LOW);
   }
 
   // User input from button has higher precendence than joystick
   // Have individual actions for either confirm or return
-  if (buttonConfirm.isPressed()) {
+  if (buttonConfirm.isPressed())
+  {
     Serial.println("Confirm button pressed");
   }
 
-  if (buttonReturn.isPressed()) {
+  if (buttonReturn.isPressed())
+  {
     Serial.println("Return button pressed");
   }
 
-  if (buttonTele.isPressed()) {
+  if (buttonTele.isPressed())
+  {
     Serial.println("Tele button pressed");
+    if (WiFi.status() == WL_CONNECTED){
+    botActive.sendMessage(CHAT_ID, "HELP! I AM IN DANGER!");
   }
 
   // Lowest priority state: only joystick input
   // Get and print joystick state and returned message
-  if (joystickUpdateTrigger) {
+  if (joystickUpdateTrigger)
+  {
     userInput = joystick.joystickReturnState();
     updateInputs();
 
@@ -122,135 +150,63 @@ void loop() {
   }
 
   // Check Wifi connection
-  if (WiFi.status() != WL_CONNECTED) {
-    // Wi-Fi is not connected LED
-    digitalWrite(ONBOARD_LED, LOW);
-    return;
-  } else {
-    // Wi-Fi is connected LED
-    digitalWrite(ONBOARD_LED, HIGH);
-    Serial.println("Checkpoint 1");
-    botController.handleUpdates();
-    Serial.println("Checkpoint 2");
-    if (buttonTele.isPressed()) {
-      bot.sendMessage(CHAT_ID, "HELP! I AM IN DANGER!");
-    }
-    /*
-    // Handling query from telegram user
-    if (millis() > lastTimeBotRan + botRequestDelay)
-    {
-      int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-
-      while (numNewMessages)
-      {
-        Serial.println("got response");
-        handleNewMessages(numNewMessages);
-        numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-      }
-      lastTimeBotRan = millis();
-    }
-
-    // Send message when button is pressed
-    if (buttonTele.isPressed())
-    {
-      Serial.println("The button is pressed");
-      bot.sendMessage(CHAT_ID, "HELP! I AM IN DANGER!");
-    }
-    else if (buttonTele.isReleased())
-    {
-      Serial.println("The button is released");
-    }
-  }
-  */
-  }
 }
+
+/*2nd Loop
+Handles all passive Telegram-related actions
+ie. polling the API for chat updates and automatically responding to them.
+Separate from the active Telegram-related actions (ie. initiated by the user.)*/
+void TelePoll(void *pvParameters)
+{
+  while (1)
+  {
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      // Wi-Fi is not connected LED
+      digitalWrite(ONBOARD_LED, LOW);
+      return;
+    }
+    else
+    {
+      // Wi-Fi is connected LED
+      digitalWrite(ONBOARD_LED, HIGH);
+      botController.handleUpdates();
+    }
+  }
 
   //------------------------------------FUNCTIONS------------------------------------
   // Function to update user state (temporary, maybe move into class later)
   // Takes user inputs and upper bound of array size (assume array is square for now?)
-  void updateInputs() {
-    for (int i = 0; i < userState.size(); i++) {
-      userState[i] = userState[i] + userInput[i];
-      if (userState[i] < 0) {
-        userState[i] = 0;
-      } else if (userState[i] > matrixSize[i]) {
-        userState[i] = matrixSize[i];
+  void updateInputs()
+  {
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      // Wi-Fi is not connected LED
+      digitalWrite(ONBOARD_LED, LOW);
+      return;
+    }
+    else
+    {
+      // Wi-Fi is connected LED
+      digitalWrite(ONBOARD_LED, HIGH);
+      Serial.println("Checkpoint 1");
+      botController.handleUpdates();
+      Serial.println("Checkpoint 2");
+      if (buttonTele.isPressed())
+      {
+        bot.sendMessage(CHAT_ID, "HELP! I AM IN DANGER!");
       }
     }
   }
 
   // Funcion to convert int vector into a string to print
-  String intToString(std::vector<int> intVector) {
+  String intToString(std::vector<int> intVector)
+  {
     std::string tempOutput = to_string(intVector[0]);
-    for (int i = 1; i < intVector.size(); i++) {
+    for (int i = 1; i < intVector.size(); i++)
+    {
       tempOutput = tempOutput + " , " + to_string(intVector[i]);
     }
     String tempOutputString = String(tempOutput.data());
     return tempOutputString;
   }
-
-  /*
-// Allow telegram to query
-void handleNewMessages(int numNewMessages)
-{
-  Serial.println("handleNewMessages");
-  Serial.println(String(numNewMessages));
-
-  for (int i = 0; i < numNewMessages; i++)
-  {
-    // Check the chat id of the requester
-    String chat_id = String(bot.messages[i].chat_id);
-    if (chat_id != CHAT_ID)
-    {
-      bot.sendMessage(chat_id, "Unauthorized user", "");
-      continue;
-    }
-
-    // Print the received message
-    String text = bot.messages[i].text;
-    Serial.println(text);
-    Serial.println(chat_id);
-    String from_name = bot.messages[i].from_name;
-
-    // Change this message accordingly to the device function
-    if (text == "/start")
-    {
-      String welcome = "Welcome, " + from_name + ".\n";
-      welcome += "Use the following commands to control your outputs.\n\n";
-      welcome += "/hello to say hello with bot \n";
-      welcome += "/name to to ask bot's name \n";
-      welcome += "/mode to ask is bot happy or not \n";
-      welcome += "/afk to ask whether user holding the device \n";
-      bot.sendMessage(chat_id, welcome, "");
-    }
-
-    if (text == "/hello")
-    {
-      bot.sendMessage(chat_id, "Hello, people", "");
-    }
-
-    if (text == "/name")
-    {
-      bot.sendMessage(chat_id, "My name is Zane Bot!", "");
-    }
-
-    if (text == "/mode")
-    {
-      bot.sendMessage(chat_id, "I am happy!", "");
-    }
-
-    // To query if the person is touching the device by questioning is the touchPinLED HIGH or LOW
-    if (text == "/afk")
-    {
-      if (digitalRead(touchPinLED))
-      {
-        bot.sendMessage(chat_id, "Person is touching the device", "");
-      }
-      else
-      {
-        bot.sendMessage(chat_id, "Person is afk", "");
-      }
-    }
-  }
-}
-*/
