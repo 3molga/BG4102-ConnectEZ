8d32d8c4d8a00a103efd0a23bc67a6eac195420a
Thad Lo <ihack2win@gmail.com>
2023-09-27 20:36:37 +0800

    Testing telebot update

--- midtermProofOfConcept/midtermProofOfConcept.ino
+++ midtermProofOfConcept/midtermProofOfConcept.ino
@@ -7,6 +7,7 @@
 #include "joystick.h"
 #include "joystickAxis.h"
 #include "ezButton.h"
+#include "telebot.h"
 #include <iostream>
 #include <vector>
 #include <string>
@@ -23,28 +24,28 @@ ezButton buttonReturn(26);
 ezButton buttonTele(25);
 #define touchPinInput 13
 #define touchPinLED 12  // External LED is at Pin 26
-#define ONBOARD_LED 2        // ON_BOARD LED value is 2
+#define ONBOARD_LED 2   // ON_BOARD LED value is 2
 
 // Wifi setup
-const char* ssid = "tmt";                                          // Wi-Fi ID
-const char* password = "yeah8913";                                 // Wi-Fi password
+const char *ssid = "tmt";                                          // Wi-Fi ID
+const char *password = "yeah8913";                                 // Wi-Fi password
 #define BOTtoken "6606670486:AAGP24iLfP047ysZ7yaAUHyv3cOf-iLKMqE"  // Telegram bot API token
 #define CHAT_ID "-4016407865"                                      // "Chat with esp32" telegram group ID
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
-int botRequestDelay = 1000;
-unsigned long lastTimeBotRan;
+telebot botController(bot);
 
 // Joystick variables
-std::vector<int> userState{ 0, 0 };  // Stores x and y coords of where the user currently is on the UI
-std::vector<int> userInput{ 0, 0 };  // Stores inputs from user
-std::vector<int> matrixSize = {3, 10};                  // Arbitrary assumption for maximum size of array (+1)
+std::vector<int> userState{ 0, 0 };       // Stores x and y coords of where the user currently is on the UI
+std::vector<int> userInput{ 0, 0 };       // Stores inputs from user
+std::vector<int> matrixSize = { 3, 10 };  // Arbitrary assumption for maximum size of array (+1)
 bool joystickUpdateTrigger;
 const String newLineBar = "-----------------------------------------------------------------------------------------------";
 
 // Capacitance touch to detect if person is afk or not
 int touchValue;                 // Variable for storing the touch pin value
 const int touchThreshold = 30;  // Capacitance touch value lower than this will indicate the person is touching the device
+bool touchBool;
 
 // -----------------------------------------SETUP-------------------------------------------
 void setup() {
@@ -86,8 +87,10 @@ void loop() {
   // Check if the touchValue is below the threshold, check if the person is touching the device
   touchValue = touchRead(touchPinInput);
   if (touchValue < touchThreshold) {
+    touchBool = TRUE;
     digitalWrite(touchPinLED, HIGH);
   } else {
+    touchBool = FALSE;
     digitalWrite(touchPinLED, LOW);
   }
 
@@ -126,11 +129,18 @@ void loop() {
   } else {
     // Wi-Fi is connected LED
     digitalWrite(ONBOARD_LED, HIGH);
+    telebot.handleUpdates();
+    if (buttonTele.isPressed()) {
+      bot.sendMessage(CHAT_ID, "HELP! I AM IN DANGER!");
+    }
+    /*
     // Handling query from telegram user
-    if (millis() > lastTimeBotRan + botRequestDelay) {
+    if (millis() > lastTimeBotRan + botRequestDelay)
+    {
       int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
-      while (numNewMessages) {
+      while (numNewMessages)
+      {
         Serial.println("got response");
         handleNewMessages(numNewMessages);
         numNewMessages = bot.getUpdates(bot.last_message_received + 1);
@@ -139,48 +149,56 @@ void loop() {
     }
 
     // Send message when button is pressed
-    if (buttonTele.isPressed()) {
+    if (buttonTele.isPressed())
+    {
       Serial.println("The button is pressed");
       bot.sendMessage(CHAT_ID, "HELP! I AM IN DANGER!");
-    } else if (buttonTele.isReleased()) {
+    }
+    else if (buttonTele.isReleased())
+    {
       Serial.println("The button is released");
     }
   }
-}
+  */
+  }
 
-//------------------------------------FUNCTIONS------------------------------------
-// Function to update user state (temporary, maybe move into class later)
-// Takes user inputs and upper bound of array size (assume array is square for now?)
-void updateInputs() {
-  for (int i = 0; i < userState.size(); i++) {
-    userState[i] = userState[i] + userInput[i];
-    if (userState[i] < 0) {
-      userState[i] = 0;
-    } else if (userState[i] > matrixSize[i]) {
-      userState[i] = matrixSize[i];
+  //------------------------------------FUNCTIONS------------------------------------
+  // Function to update user state (temporary, maybe move into class later)
+  // Takes user inputs and upper bound of array size (assume array is square for now?)
+  void updateInputs() {
+    for (int i = 0; i < userState.size(); i++) {
+      userState[i] = userState[i] + userInput[i];
+      if (userState[i] < 0) {
+        userState[i] = 0;
+      } else if (userState[i] > matrixSize[i]) {
+        userState[i] = matrixSize[i];
+      }
     }
   }
-}
 
-// Funcion to convert int vector into a string to print
-String intToString(std::vector<int> intVector) {
-  std::string tempOutput = to_string(intVector[0]);
-  for (int i = 1; i < intVector.size(); i++) {
-    tempOutput = tempOutput + " , " + to_string(intVector[i]);
+  // Funcion to convert int vector into a string to print
+  String intToString(std::vector<int> intVector) {
+    std::string tempOutput = to_string(intVector[0]);
+    for (int i = 1; i < intVector.size(); i++) {
+      tempOutput = tempOutput + " , " + to_string(intVector[i]);
+    }
+    String tempOutputString = String(tempOutput.data());
+    return tempOutputString;
   }
-  String tempOutputString = String(tempOutput.data());
-  return tempOutputString;
-}
 
+  /*
 // Allow telegram to query
-void handleNewMessages(int numNewMessages) {
+void handleNewMessages(int numNewMessages)
+{
   Serial.println("handleNewMessages");
   Serial.println(String(numNewMessages));
 
-  for (int i = 0; i < numNewMessages; i++) {
+  for (int i = 0; i < numNewMessages; i++)
+  {
     // Check the chat id of the requester
     String chat_id = String(bot.messages[i].chat_id);
-    if (chat_id != CHAT_ID) {
+    if (chat_id != CHAT_ID)
+    {
       bot.sendMessage(chat_id, "Unauthorized user", "");
       continue;
     }
@@ -192,7 +210,8 @@ void handleNewMessages(int numNewMessages) {
     String from_name = bot.messages[i].from_name;
 
     // Change this message accordingly to the device function
-    if (text == "/start") {
+    if (text == "/start")
+    {
       String welcome = "Welcome, " + from_name + ".\n";
       welcome += "Use the following commands to control your outputs.\n\n";
       welcome += "/hello to say hello with bot \n";
@@ -202,25 +221,33 @@ void handleNewMessages(int numNewMessages) {
       bot.sendMessage(chat_id, welcome, "");
     }
 
-    if (text == "/hello") {
+    if (text == "/hello")
+    {
       bot.sendMessage(chat_id, "Hello, people", "");
     }
 
-    if (text == "/name") {
+    if (text == "/name")
+    {
       bot.sendMessage(chat_id, "My name is Zane Bot!", "");
     }
 
-    if (text == "/mode") {
+    if (text == "/mode")
+    {
       bot.sendMessage(chat_id, "I am happy!", "");
     }
 
     // To query if the person is touching the device by questioning is the touchPinLED HIGH or LOW
-    if (text == "/afk") {
-      if (digitalRead(touchPinLED)) {
+    if (text == "/afk")
+    {
+      if (digitalRead(touchPinLED))
+      {
         bot.sendMessage(chat_id, "Person is touching the device", "");
-      } else {
+      }
+      else
+      {
         bot.sendMessage(chat_id, "Person is afk", "");
       }
     }
   }
 }
+*/
---
+++ midtermProofOfConcept/telebot.cpp
@@ -0,0 +1,99 @@
+// Source file for telegram bot behaviour
+
+#include <Arduino.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+// Private Variables
+const int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+int numNewMessages;
+
+// External pins
+extern bool touchBool;
+
+//---------------------------------PUBLIC---------------------------------
+// Constructor
+telebot::telebot(UniversalTelegramBot &bot) : universalTelegramBot(bot)
+{
+}
+
+// handleUpdates
+// Queries updates from Telegram and handles them
+void telebot::handleUpdates()
+{
+	if (millis() > lastTimeBotRan + botRequestDelay)
+	{
+		numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+		while (numNewMessages)
+		{
+			telebot.handleNewMessages(numNewMessages);
+			numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+		}
+		lastTimeBotRan = millis();
+	}
+}
+
+void telebot::handleNewMessages(int numNewMessages)
+{
+  for (int i = 0; i < numNewMessages; i++)
+  {
+    // Check the chat id of the requester
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID)
+    {
+      bot.sendMessage(chat_id, "Unauthorized user", "");
+      continue;
+    }
+
+    // Print the received message to strings
+    String text = bot.messages[i].text;
+    String from_name = bot.messages[i].from_name;
+
+    // Change this message accordingly to the device function
+    if (text == "/start")
+    {
+      String welcome = "Welcome, " + from_name + ".\n";
+      welcome += "Use the following commands to control your outputs.\n\n";
+      welcome += "/hello to say hello with bot \n";
+      welcome += "/name to to ask bot's name \n";
+      welcome += "/mode to ask is bot happy or not \n";
+      welcome += "/afk to ask whether user holding the device \n";
+      bot.sendMessage(chat_id, welcome, "");
+    }
+
+    if (text == "/hello")
+    {
+      bot.sendMessage(chat_id, "Hello, people", "");
+	  return;
+    }
+
+    if (text == "/name")
+    {
+      bot.sendMessage(chat_id, "My name is Zane Bot!", "");
+	  return;
+    }
+
+    if (text == "/mode")
+    {
+      bot.sendMessage(chat_id, "I am happy!", "");
+	  return;
+    }
+
+    // To query if the person is touching the device by questioning is the touchPinLED HIGH or LOW
+    if (text == "/afk")
+    {
+      if (touchBool)
+      {
+        bot.sendMessage(chat_id, "Person is touching the device", "");
+	  	return;
+      }
+      else
+      {
+        bot.sendMessage(chat_id, "Person is afk", "");
+	  	return;
+      }
+    }
+  }
+}
